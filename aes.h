00001e08352e9bee0296e4f91b29d97700d68e64
kokke <spam@rowdy.dk>
2014-07-12 02:12:01 +0200

    Update aes.h

--- aes.h
+++ aes.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void AES128_ECB_encrypt(uint8_t* input, uint8_t* key, uint8_t *output);
+void AES128_ECB_decrypt(uint8_t* input, uint8_t* key, uint8_t *output);
 
 #endif //_AES_H_
