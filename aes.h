15adafae4d062601fe0473388e98621fc53f65f4
namreeb <github@namreeb.org>
2015-08-06 21:12:02 -1000

    const'ify some parameters

--- aes.h
+++ aes.h
@@ -22,8 +22,8 @@
 
 #if defined(ECB) && ECB
 
-void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
-void AES128_ECB_decrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_encrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_decrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
 
 #endif // #if defined(ECB) && ECB
 
