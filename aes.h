a691dbd55f3d3716f2f74810048eb90ab72a7cd0
Djones A. Boni <djboni@gmail.com>
2016-09-09 19:25:16 -0300

    Const input in ECB mode

--- aes.h
+++ aes.h
@@ -26,8 +26,8 @@ extern "C" {
 
 #if defined(ECB) && ECB
 
-void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
-void AES128_ECB_decrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_encrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_decrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
 
 #endif /* #if defined(ECB) && ECB */
 
