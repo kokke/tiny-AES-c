5e2f7a88b386213062746646e3e6270ac5040fa0
arne krüger <ak@crystal-photonics.com>
2016-11-03 15:34:50 +0100

    -fixes #20 "Multiblock CBC decryption failed"
    https://github.com/kokke/tiny-AES128-C/issues/20

--- aes.h
+++ aes.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 // #define the macros below to 1/0 to enable/disable the mode of operation.
 //
@@ -34,6 +38,10 @@ void AES128_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length,
 void AES128_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
 
 #endif // #if defined(CBC) && CBC
+
+#ifdef __cplusplus
+}
+#endif
 
 
 
