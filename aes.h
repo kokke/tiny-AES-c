c26fb6a5ec330fd790808126f272cb2daa2a7617
Matteo Brichese <matteo.brichese@wunderbar.com>
2017-06-05 17:07:34 -0700

    adding Key Expansion for AES192 and AES256

--- aes.h
+++ aes.h
@@ -19,22 +19,20 @@
 #endif
 
 
-
 #if defined(ECB) && ECB
 
-void AES128_ECB_encrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
-void AES128_ECB_decrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES_ECB_encrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES_ECB_decrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
 
 #endif // #if defined(ECB) && ECB
 
 
 #if defined(CBC) && CBC
 
-void AES128_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
-void AES128_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+void AES_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+void AES_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
 
 #endif // #if defined(CBC) && CBC
-
 
 
 #endif //_AES_H_
