0ae73a590122f44ac9809d3a45060c384aedbb51
Amlan-007 <56549024+Amlan-007@users.noreply.github.com>
2019-10-24 19:17:40 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-
+//close order
 // Enable ECB, CTR and CBC mode. Note this can be done before including aes.h or at compile-time.
 // E.g. with GCC by using the -D flag: gcc -c aes.c -DCBC=0 -DCTR=1 -DECB=1
 #define CBC 1
