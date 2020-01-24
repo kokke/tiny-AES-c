f9fc97a6e4af39e410ddd25a53e569c6cf982894
kokke <spam@rowdy.dk>
2020-01-24 16:54:19 +0100

    Update aes.c

--- aes.c
+++ aes.c
@@ -35,7 +35,6 @@ NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
 /*****************************************************************************/
 /* Includes:                                                                 */
 /*****************************************************************************/
-#include <stdint.h>
 #include <string.h> // CBC mode, for memset
 #include "aes.h"
 
